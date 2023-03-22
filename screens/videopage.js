5c11b9778e77cba07e62b57dfcbfddcbdf99875b
AlleeElias <eliasneel2001@gmail.com>
2023-03-22 19:08:46 +0100

    working on video

--- screens/videopage.js
+++ screens/videopage.js
@@ -1,7 +1,25 @@
-import {Text} from "react-native";
+import {Text, StyleSheet} from "react-native";
+import Video from "react-native-video";
 
-export function videopage(){
+export function videopage() {
     return (<>
-        <Text>Dit is de videopagina!</Text>
+        <Text>Geniet van deze prachtige video!</Text>
+        <Video source={{uri: "beelden"}}   // Can be a URL or a local file.
+               ref={(ref) => {
+                   this.player = ref
+               }}                                      // Store reference
+               onBuffer={this.onBuffer}                // Callback when remote video is buffering
+               onError={this.videoError}               // Callback when video cannot be loaded
+               style={styles.backgroundVideo}/>
     </>);
 }
+
+var styles = StyleSheet.create({
+    backgroundVideo: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+    },
+});
