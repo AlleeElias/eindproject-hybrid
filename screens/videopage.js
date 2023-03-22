d25b21142b2c53db69b51394333ebcb7fad6c5b6
AlleeElias <eliasneel2001@gmail.com>
2023-03-22 19:15:01 +0100

    video working

--- screens/videopage.js
+++ screens/videopage.js
@@ -1,25 +1,21 @@
-import {Text, StyleSheet} from "react-native";
-import Video from "react-native-video";
+import {Text, Linking} from "react-native";
+import WebView from "react-native-webview";
 
 export function videopage() {
+    const uri = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+
     return (<>
         <Text>Geniet van deze prachtige video!</Text>
-        <Video source={{uri: "beelden"}}   // Can be a URL or a local file.
-               ref={(ref) => {
-                   this.player = ref
-               }}                                      // Store reference
-               onBuffer={this.onBuffer}                // Callback when remote video is buffering
-               onError={this.videoError}               // Callback when video cannot be loaded
-               style={styles.backgroundVideo}/>
+        <WebView
+            source={{uri}}
+            onShouldStartLoadWithRequest={(request) => {
+                if (request.url !== uri) {
+                    Linking.openURL(request.url);
+                    return false;
+                }
+
+                return true;
+            }}
+        />
     </>);
 }
-
-var styles = StyleSheet.create({
-    backgroundVideo: {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        bottom: 0,
-        right: 0,
-    },
-});
